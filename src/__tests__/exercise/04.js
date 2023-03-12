463cf5a7341336d38c80d999831e2dd0dcef97b5
Alberto Santacruz <mirasmar.dev@gmail.com>
2023-03-12 12:15:53 +0100

    React-test: E4 Extra1

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -7,12 +7,7 @@ import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
 test('submitting the form calls onSubmit with username and password', async () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
+  const handleSubmit = jest.fn()
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
   render(<Login onSubmit={handleSubmit} />)
   // 🐨 get the username and password fields via `getByLabelText`
@@ -25,12 +20,12 @@ test('submitting the form calls onSubmit with username and password', async () =
   await userEvent.type(screen.getByLabelText(/password/i), password)
   // 🐨 click on the button with the text "Submit"
   await userEvent.click(screen.getByRole('button', {name: /submit/i}))
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
-  expect(submittedData).toEqual({
+
+  expect(handleSubmit).toHaveBeenCalledWith({
     username,
     password,
   })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
 
 /*
