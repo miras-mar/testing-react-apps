e80108e65c52cacd31ac214d69ce99f024a6a5f6
Alberto Santacruz <mirasmar.dev@gmail.com>
2023-03-12 12:52:42 +0100

    React-test: E5 Extra2

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -42,3 +42,16 @@ test(`logging in displays the user's username`, async () => {
   // 🐨 assert that the username is on the screen
   expect(screen.getByText(username)).toBeInTheDocument()
 })
+
+test('omitting the password results in an error', async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  // don't type in the password
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent('password required')
+})
